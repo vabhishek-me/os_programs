105e0c0e76da28ce1962edfa4dd8347f5cea7f22
Abhishek Vishwakarma <abhishek.k.v76076@gmail.com>
2018-04-06 01:16:38 +0530

    sum_using_multiple_threads.c added

--- Multithreaded Program/sum_using_multiple_threads.c
+++ Multithreaded Program/sum_using_multiple_threads.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
+
+long long sum;
+void* sumFunc(void *);
+
+struct sumFunc_struct {
+	long long limit;
+	long long lsum;
+};
 
 int main(int argc, char **argv) {
 	if (argc < 2) {
-		printf("\nUsage: %s <num 1> <num 2> .... <num n>\n\n", argv[0]);
+		printf("\n Usage: %s <num 1> <num 2> .... <num n>\n\n", argv[0]);
+		exit(-1);
 	}
+
+	int nums = argc - 1;
+
+	struct sumFunc_struct toSum[nums];
+	pthread_t sumThreads[nums];
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+
+	for(int i=0; i < nums; i++) {
+		toSum[i].limit = atoll(argv[i+1]);
+		pthread_create(&sumThreads[i], &attr, sumFunc, &toSum[i]);
+	}
+
+	for(int i=0; i < nums; i++) {
+		pthread_join(sumThreads[i], NULL);
+		printf("The sum upto %lld is %lld\n", toSum[i].limit, toSum[i].lsum);
+	}
+}
+
+void* sumFunc(void* arg) {
+	struct sumFunc_struct* data = (struct sumFunc_struct*) arg;
+
+	sum = 0;
+	for(long long i=1; i<=data->limit; i++)
+		sum += i;
+	data->lsum = sum;	
+	pthread_exit(0);
 }
