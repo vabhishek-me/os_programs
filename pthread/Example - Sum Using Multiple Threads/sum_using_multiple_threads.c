2d249d04476bfb5d7762bf7569ed2c33fd4208e6
Abhishek Vishwakarma <abhishek.k.v76076@gmail.com>
2018-04-12 14:43:06 +0530

    reorganized

--- pthread/Example - Sum Using Multiple Threads/sum_using_multiple_threads.c
+++ pthread/Example - Sum Using Multiple Threads/sum_using_multiple_threads.c
@@ -5,10 +5,11 @@
 long long sum;
 void* sumFunc(void *);
 
-struct sumFunc_struct {
+typedef struct sumFunc_thread_data {
+	pthread_t thread;
 	long long limit;
 	long long lsum;
-};
+} SUM_THREAD;
 
 int main(int argc, char **argv) {
 	if (argc < 2) {
@@ -18,28 +19,27 @@ int main(int argc, char **argv) {
 
 	int nums = argc - 1;
 
-	struct sumFunc_struct toSum[nums];
-	pthread_t sumThreads[nums];
+	SUM_THREAD *sumThreads = (SUM_THREAD *)malloc(nums * sizeof(*sumThreads));
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
 
 	for(int i=0; i < nums; i++) {
-		toSum[i].limit = atoll(argv[i+1]);
-		pthread_create(&sumThreads[i], &attr, sumFunc, &toSum[i]);
+		sumThreads[i].limit = atoll(argv[i+1]);
+		pthread_create(&sumThreads[i].thread, &attr, sumFunc, (void *)&sumThreads[i]);
 	}
 
 	for(int i=0; i < nums; i++) {
-		pthread_join(sumThreads[i], NULL);
-		printf("The sum upto %lld is %lld\n", toSum[i].limit, toSum[i].lsum);
+		pthread_join(sumThreads[i].thread, NULL);
+		printf("The sum upto %lld is %lld\n", sumThreads[i].limit, sumThreads[i].lsum);
 	}
 }
 
 void* sumFunc(void* arg) {
-	struct sumFunc_struct* data = (struct sumFunc_struct*) arg;
+	SUM_THREAD* data = (SUM_THREAD *) arg;
 
 	sum = 0;
 	for(long long i=1; i<=data->limit; i++)
 		sum += i;
 	data->lsum = sum;	
-	pthread_exit(0);
+	pthread_exit(NULL);
 }
